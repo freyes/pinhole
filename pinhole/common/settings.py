f82e8aa308e9b0763cf1d61126efe9557ae1e16e
Felipe Reyes <freyes@tty.cl>
2013-08-13 03:46:06 +0000

    Droped hardcoded creds and bucket configs in favor of environment variables
    Environment variables are PaaS friendly, so it's the way to go

--- pinhole/common/settings.py
+++ pinhole/common/settings.py
@@ -1,16 +1,16 @@
 from os import environ
 
-DB_URI = 'sqlite:////tmp/pinhole_unittests.db'
+DB_URI = environ.get("PINHOLE_DB_URI", 'sqlite:////tmp/pinhole_unittests.db')
 
 # configuration
 DEBUG = True
 SECRET_KEY = 'development key'
 SQLALCHEMY_DATABASE_URI = DB_URI
 
-AWS_ACCESS_KEY = "AKIAIWNMBKL4DUKXLWBQ"
-AWS_SECRET_KEY = "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"
-PHOTO_BUCKET = "storage.pinhole.tty.cl"
-INCOMING_PHOTO_BUCKET = "dev.pinhole.tty.cl"
+AWS_ACCESS_KEY = environ["AWS_ACCESS_KEY"]
+AWS_SECRET_KEY = environ["AWS_SECRET_KEY"]
+PHOTO_BUCKET = environ["PINHOLE_PHOTO_BUCKET"]
+INCOMING_PHOTO_BUCKET = environ["PINHOLE_INCOMING_PHOTO_BUCKET"]
 
 # ---------------------------------------------
 # Celery
